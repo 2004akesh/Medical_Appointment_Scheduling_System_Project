7f9d1d0bdacefd4bd78bd10f72069264740707e3
IT25101382 <it25101382@my.sliit.lk>
2026-04-27 12:48:00 +0530

    Admin java class

---
+++ src/main/java/com/healthcare/healthcaremanagement/admin/Admin.java
@@ -0,0 +1,31 @@
+package com.healthcare.healthcaremanagement.admin;
+
+import com.healthcare.healthcaremanagement.Person;
+
+// Inheritance - Admin extends Person
+public class Admin extends Person {
+    private String username;
+    private String password;
+    private String role;
+
+    public Admin() {}
+
+    public Admin(String id, String name, String phone,
+                 String username, String password, String role) {
+        super(id, name, phone);
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    // Polymorphism
+    @Override
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+}
